e42ec4c9e7a358af7fdf12df8b65faa5a887de97
Sitch196 <sichinava1996@gmail.com>
2025-05-12 23:58:37 +0400

    fix : build fail fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,10 @@ import { UsersModule } from './users/users.module';
 import { ShipsModule } from './ships/ships.module';
 import { DocksModule } from './docks/docks.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { Users } from './users/user.entity';
+import { Ship } from './ships/ship.entity';
+import { Dock } from './docks/entities/dock.entity';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 @Module({
   imports: [
@@ -15,32 +19,29 @@ import { NotificationsModule } from './notifications/notifications.module';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        const isProduction = configService.get('NODE_ENV') === 'production';
+      useFactory: (configService: ConfigService): PostgresConnectionOptions => {
+        const isDevelopment = configService.get('NODE_ENV') !== 'production';
         const databaseUrl = configService.get('DATABASE_URL');
-
-        if (databaseUrl && isProduction) {
-          return {
-            type: 'postgres',
-            url: databaseUrl,
-            autoLoadEntities: true,
-            entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: false,
-            ssl: { rejectUnauthorized: false },
-          };
-        }
 
         return {
           type: 'postgres',
-          host: configService.get('DB_HOST', 'localhost'),
-          port: configService.get('DB_PORT', 5432),
-          username: configService.get('DB_USERNAME', 'postgres'),
-          password: configService.get('DB_PASSWORD', 'Jme24in!'),
-          database: configService.get('DB_NAME', 'postgres'),
-          autoLoadEntities: true,
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          synchronize: false,
-        };
+          ...(databaseUrl
+            ? {
+                url: databaseUrl,
+                ssl: {
+                  rejectUnauthorized: false,
+                },
+              }
+            : {
+                host: configService.get('DB_HOST', 'localhost'),
+                port: parseInt(configService.get('DB_PORT', '5432')),
+                username: configService.get('DB_USERNAME', 'postgres'),
+                password: configService.get('DB_PASSWORD'),
+                database: configService.get('DB_NAME', 'postgres'),
+              }),
+          entities: [Users, Ship, Dock],
+          synchronize: isDevelopment,
+        } as PostgresConnectionOptions;
       },
     }),
     AuthModule,
